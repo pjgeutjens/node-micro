d6900cb3dc240a235cf7171738e002b2880e9704
Pieter Jan Geutjens <pieter.geutjens@leapconsulting.be>
2022-09-23 07:31:39 +0200

    sync client

--- ticketing/client/pages/auth/signout.js
+++ ticketing/client/pages/auth/signout.js
@@ -1,15 +1,18 @@
-import { useEffect } from "react";
-import Router from "next/router";
-import useRequest from "../../hooks/use-request";
+import { useEffect } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
 
-export default () => { 
+export default () => {
   const { doRequest } = useRequest({
     url: '/api/users/signout',
     method: 'post',
     body: {},
     onSuccess: () => Router.push('/')
-  })
+  });
 
-  useEffect(() => { doRequest() , []})
-  return <div>Signing you out...</div>
- }
+  useEffect(() => {
+    doRequest();
+  }, []);
+
+  return <div>Signing you out...</div>;
+};
